Fixed Cross Entropy Formula

ProyectoNumeros/CrossEntropyLoss.py
@@ -11,7 +11,7 @@ import numpy as np
 class CrossEntropyLoss:
     def compute_loss(self, y_true, y_pred):
         # 
-        return -np.mean(np.sum(y_true * np.log(y_pred) + ((1 - y_true)*np.log(1-y_pred))))
-
+        # return -np.mean(np.sum(y_true * np.log(y_pred) + ((1 - y_true)*np.log(1-y_pred))))
+        return -np.mean(np.sum(y_true * np.log(y_pred + 1e-9), axis=1))
     def compute_gradient(self, y_true, y_pred):
         return (y_pred - y_true) / y_true.shape[0]